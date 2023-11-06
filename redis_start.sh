80ca1af6faf3831e5a4b3ec3f6f0a2b6679ab529
Eunseon Kang <eunseon.kang@probit.kr>
2023-11-06 17:51:46 +0900

    redis cluster 설정

---
+++ redis_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+redis-server ./redis_6300.conf
+redis-server ./redis_6301.conf
+redis-server ./redis_6302.conf
+redis-server ./redis_6400.conf
+redis-server ./redis_6401.conf
+redis-server ./redis_6402.conf
